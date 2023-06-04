d41b670429cd87f3eb4dae3555a26f136b66be52
Elijah <elijah.ivanik@inbox.ru>
2023-06-04 10:24:23 +0300

    feat: implement Theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Link } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import ThemeProvider from "./theme/ThemeProvider";
+import { classNames } from "./helpers/classNames/classNames";
+import { useTheme } from "./theme/useTheme";
+import "./styles/index.scss";
 
-const App = () => {
+const App: React.FC = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <>
+    <div className={classNames("app", {}, [theme])}>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
+
+      <button onClick={toggleTheme}>TOGGLE</button>
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
@@ -24,7 +32,7 @@ const App = () => {
           />
         </Routes>
       </Suspense>
-    </>
+    </div>
   );
 };
 
@@ -32,6 +40,8 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
 );
