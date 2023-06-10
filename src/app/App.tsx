69860996ad9b37eaa7de785e0af5fe7e8b85126c
Elijah <elijah.ivanik@inbox.ru>
2023-06-10 08:31:40 +0300

    feat: implement Loaders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,20 @@
-import { Link } from "react-router-dom";
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProvider";
-import { AppRouter } from "./providers/router";
+import React from 'react';
+import './styles/index.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
-import "./styles/index.scss";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+   const { theme } = useTheme();
 
-  return (
-    <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-
-      <AppRouter />
-    </div>
-  );
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
+        </div>
+    );
 };
 
 export default App;
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,18 +1,25 @@
-import type { RouteProps } from "react-router-dom";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import {RouteProps} from "react-router-dom";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
 
-export enum APP_ROUTES {
-  "MAIN" = "main",
-  "ABOUT" = "about"
+
+export enum AppRoutes {
+    MAIN = 'main',
+    ABOUT = 'about',
 }
 
-export const RoutePath: Record<APP_ROUTES, string> = {
-  [APP_ROUTES.MAIN]: "/",
-  [APP_ROUTES.ABOUT]: "/about"
-};
+export const RoutePath: Record<AppRoutes, string> = {
+    [AppRoutes.MAIN]: '/',
+    [AppRoutes.ABOUT]: '/about',
+}
 
-export const routeConfig: RouteProps[] = [
-  { path: RoutePath.main, element: <MainPage /> },
-  { path: RoutePath.about, element: <AboutPage /> }
-];
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+    [AppRoutes.MAIN]: {
+        path: RoutePath.main,
+        element: <MainPage />
+    },
+    [AppRoutes.ABOUT]: {
+        path: RoutePath.about,
+        element: <AboutPage />
+    },
+}
