779f87164ce49318d4bfb079ba9a9dc96328e0b0
Elijah <elijah.ivanik@inbox.ru>
2023-07-08 13:27:28 +0300

    feat: implement micro frontends

--- mine/config/build/buildPlugins.ts
+++ mine/config/build/buildPlugins.ts
@@ -2,12 +2,33 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const deps = require("../../package.json").dependencies;
 const { ModuleFederationPlugin } = require("webpack").container;
 
 export function buildPlugins({
   paths
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
+    new ModuleFederationPlugin({
+      name: "ulbi",
+      filename: "remoteEntry.js",
+      remotes: {
+        ulbi: "ulbi@http://localhost:3000/remoteEntry.js"
+      },
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"]
+        }
+      }
+    }),
     new HtmlWebpackPlugin({
       template: paths.html
     }),
--- ulbi/config/build/buildPlugins.ts
+++ ulbi/config/build/buildPlugins.ts
@@ -2,12 +2,33 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const deps = require("../../package.json").dependencies;
 const { ModuleFederationPlugin } = require("webpack").container;
 
 export function buildPlugins({
   paths
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
+    new ModuleFederationPlugin({
+      name: "ulbi",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./Navbar": "./src/widgets/Navbar"
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"]
+        }
+      }
+    }),
     new HtmlWebpackPlugin({
       template: paths.html
     }),
